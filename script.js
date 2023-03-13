bdb55b30f88331234ae08cd8f498286ec5b6b1d7
Luciano Henrique <luciano.castellucy@gmail.com>
2023-03-13 01:11:53 -0300

    código 2, para teste

--- script.js
+++ script.js
@@ -1,33 +1,83 @@
-function adicionarCampos() {
-  const numCampos = document.getElementById("num-campos").value;
-  let htmlCampos = "";
-  for (let i = 1; i <= numCampos; i++) {
-    htmlCampos += `<label for="campo-${i}">Campo ${i}:</label>`;
-    htmlCampos += `<input type="text" id="campo-${i}" name="campo-${i}" required>`;
-  }
-  const divCampos = document.getElementById("campos");
-  divCampos.innerHTML = htmlCampos;
-}
+const form = document.querySelector("form");
+const itensDiv = document.querySelector("#itens");
 
-function gerarNomes() {
-  const numCampos = document.getElementById("num-campos").value;
-  const valores = [];
-  for (let i = 1; i <= numCampos; i++) {
-    const campo = document.getElementById(`campo-${i}`).value;
-    valores.push(campo);
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  itensDiv.innerHTML = "";
+
+  const numItens = parseInt(document.querySelector("#num_itens").value);
+
+  for (let i = 1; i <= numItens; i++) {
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("item");
+
+    const label = document.createElement("label");
+    label.textContent = `Item ${i}:`;
+
+    const input = document.createElement("input");
+    input.type = "number";
+    input.min = "1";
+    input.name = `item_${i}`;
+    input.required = true;
+
+    itemDiv.appendChild(label);
+    itemDiv.appendChild(input);
+    itensDiv.appendChild(itemDiv);
   }
-  const nomes = [];
-  for (let i = 0; i < Math.max(...valores); i++) {
-    for (let j = 0; j < valores.length; j++) {
-      if (i < valores[j]) {
-        nomes.push(`Campo ${j + 1}`);
-      }
+
+  const gerarPlanilhaBtn = document.createElement("button");
+  gerarPlanilhaBtn.type = "button";
+  gerarPlanilhaBtn.textContent = "Gerar planilha";
+  gerarPlanilhaBtn.addEventListener("click", gerarPlanilha);
+
+  itensDiv.appendChild(gerarPlanilhaBtn);
+});
+
+function gerarPlanilha() {
+  const itens = [];
+
+  // Adiciona cada item na quantidade informada
+  for (
+    let i = 1;
+    i <= parseInt(document.querySelector("#num_itens").value);
+    i++
+  ) {
+    const quantidade = parseInt(
+      document.querySelector(`input[name=item_${i}]`).value
+    );
+    for (let j = 0; j < quantidade; j++) {
+      itens.push(`Item ${i}`);
     }
   }
-  const tabela = document.getElementById("tabela-resultados");
-  let htmlTabela = "";
-  for (let i = 0; i < nomes.length; i++) {
-    htmlTabela += `<tr><td>${i + 1}</td><td>${nomes[i]}</td></tr>`;
+
+  // Embaralha os itens
+  for (let i = itens.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [itens[i], itens[j]] = [itens[j], itens[i]];
   }
-  tabela.innerHTML = htmlTabela;
+
+  // Cria a tabela com os itens distribuídos
+  const table = document.createElement("table");
+
+  const headerRow = document.createElement("tr");
+  const headerCell = document.createElement("th");
+  headerCell.textContent = "Itens distribuídos";
+  headerCell.colSpan = 2;
+  headerRow.appendChild(headerCell);
+  table.appendChild(headerRow);
+
+  let currentRow;
+  for (let i = 0; i < itens.length; i++) {
+    if (i % 2 === 0) {
+      currentRow = document.createElement("tr");
+      currentRow.dataset.item = itens[i];
+      table.appendChild(currentRow);
+    }
+
+    const cell = document.createElement("td");
+    cell.textContent = itens[i];
+    currentRow.appendChild(cell);
+  }
+
+  itensDiv.appendChild(table);
 }
